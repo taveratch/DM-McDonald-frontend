e61cc5b960806a39f4fb9f785cf21eaea5e58c4c
raksit <rak-sit@hotmail.com>
2017-05-04 23:14:15 +0700

    basic web structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,58 @@
 import React from 'react';
 import NavBar from './nav';
+import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor';
 import './style.css';
 
 class App extends React.Component {
+
+	componentWillMount() {
+		configureAnchors({scrollDuration: 1000});
+	}
+
 	render() {
 		return (
       <div className="App">
         <NavBar />
-        <div>
-					<h1>McDonald's Healthy Meal Generator</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
+				<div className="container">
+					<div className="section-greeting">
+						<ScrollableAnchor id={'greeting'}>
+							<div>
+								<h1>McDonald's Healthy Meal Generator</h1>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+								<div className="row">
+									<div className="col-md-offset-4 col-md-2">
+										<select className="form-control">
+											<option value="" disabled selected>Select your gender</option>
+											<option value="male">Male</option>
+											<option value="female">Female</option>
+										</select>
+									</div>
+
+									<div className="col-md-2">
+										<select className="form-control">
+											<option value="" disabled selected>Select your age</option>
+											<option value="infant">Less than 4 y</option>
+											<option value="child">4-8 y</option>
+											<option value="adult">30-35 y</option>
+											<option value="elder">More than 50 y</option>
+										</select>
+									</div>
+								</div>
+
+								<a className="btn btn-default btn-generate" href="#meal">Generate</a>
+							</div>
+						</ScrollableAnchor>
+					</div>
+
+					<div className="section-meal">
+						<ScrollableAnchor id={'meal'}>
+							<div>
+								<h1>Your Meal</h1>
+							</div>
+						</ScrollableAnchor>
+					</div>
+				</div>
       </div>
     );
 	}
--- src/components/App/nav.js
+++ src/components/App/nav.js
@@ -4,7 +4,7 @@ import { Navbar } from 'react-bootstrap'; // eslint-disable-line
 class NavBar extends React.Component {
 	render() {
 		return (
-      <Navbar>
+      <Navbar className="navbar navbar-default navbar-fixed-top">
         <Navbar.Header>
           <Navbar.Brand>
             <a href="#">Brand</a>
@@ -13,10 +13,10 @@ class NavBar extends React.Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Navbar.Text>
-            Signed in as: <Navbar.Link href="#">Mark Otto</Navbar.Link>
+            {/* Signed in as: <Navbar.Link href="#">Mark Otto</Navbar.Link> */}
           </Navbar.Text>
           <Navbar.Text pullRight>
-            Have a great day!
+            <a href="#">About</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
