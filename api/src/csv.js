ae684a634679a67795f5eafd6f3dda384db80f69
TAWEESOFT <taweesoft@TAWEESOFTs-MacBook-Pro.local>
2017-05-08 09:55:36 +0700

    returns facts based on gender and age

--- api/src/csv.js
+++ api/src/csv.js
@@ -7,6 +7,8 @@ const normalize = require('./normalize.js');
 const _ = require('lodash');
 const csvAPI = express();
 
+const GENDERS = ['male', 'female'];
+
 let readCSV = (file) => {
 	let results = [];
 	return new Promise((resolve, reject) => {
@@ -20,39 +22,58 @@ let readCSV = (file) => {
 
 csvAPI.get('/data', async (req, res) => {
 	console.log('Getting data');
+
 	let file = __dirname + '/data/menu.csv';
 	let results = await readCSV(file);
+
 	res.send(results);
 });
 
 csvAPI.get('/facts', async (req, res) => {
 	console.log('Getting facts');
-	let file = __dirname + '/data/facts.csv';
-	let results = await readCSV(file);
-	res.send(results);
+
+	let gender = _.lowerCase(req.query['gender']) || 'male';
+	gender = _.includes(GENDERS, gender) ? gender : 'male'; //Verify the input
+
+	let age = parseInt(req.query['age']) || 20;
+
+	let file = __dirname + `/data/facts-${gender}.csv`;
+	let facts = await readCSV(file);
+
+	let factFromAge = findFactFromAge(facts, age);
+
+	res.send(factFromAge);
 });
 
 csvAPI.get('/centroids', async (req, res) => {
 	console.log('Getting Centroids');
+
 	let file = __dirname + '/data/centroids.csv';
 	let results = await readCSV(file);
+
 	res.send(results);
 });
 
 csvAPI.get('/user/centroid', async (req, res) => {
 	console.log('Getting user centroid');
-	let sex = req.query['sex'] || 'male'; //male by default
+
+	let gender = _.lowerCase(req.query['gender']) || 'male'; //male by default
+	gender = _.includes(GENDERS, gender) ? gender : 'male'; //Verify the input, male by default
 	let age = parseInt(req.query['age']) || 20;
+
 	let dataLocation = __dirname + '/data/menu.csv';
-	let factsLocation = __dirname + `/data/facts-${sex}.csv`;
+	let factsLocation = __dirname + `/data/facts-${gender}.csv`;
 	let centroidsLocation = __dirname + '/data/centroids.csv';
 	let results = await readCSV(dataLocation);
 	let facts = await readCSV(factsLocation);
 	let centroids = await readCSV(centroidsLocation);
+
 	let fact = findFactFromAge(facts, age);
+
 	let userCentroid = normalize.normalize(results, fact); // male
 	let cluster = normalize.getCluster(centroids, userCentroid);
 	let clusterName = normalize.getClusterName(cluster);
+
 	res.send({ 'user_centroid' : userCentroid, 'cluster' : cluster, 'name' : clusterName });
 });
 
---
+++ api/src/food-picker.js
@@ -0,0 +1,5 @@
+module.exports = {
+	pick: (meal, food) => {
+
+	}
+};
